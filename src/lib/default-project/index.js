6c121c648e9eb8d0dd0f62a8fb15aa91ed57f72d
RyaninCn11 <148440803+RyaninChina11@users.noreply.github.com>
2026-05-10 22:21:55 +0800

    update translations & add Tips function

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -2,9 +2,15 @@ import projectData from './project-data';
 
 /* eslint-disable import/no-unresolved */
 import overrideDefaultProject from '!arraybuffer-loader!./override-default-project.sb3';
-import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
+import backdrop from '!raw-loader!./223d25c297317463755043f276178aef.svg';
 /* eslint-enable import/no-unresolved */
 import {TextEncoder} from '../tw-text-encoder';
+import tips from './tips.json';
+
+function genTip(str) {
+    const randomTip = tips[Math.floor(Math.random() * tips.length)];
+    return str.replace(/\$\{TIP\}/g, randomTip);
+}
 
 const defaultProject = translator => {
     if (overrideDefaultProject.byteLength > 0) {
@@ -31,10 +37,10 @@ const defaultProject = translator => {
         dataFormat: 'JSON',
         data: JSON.stringify(projectJson)
     }, {
-        id: 'cd21514d0531fdffb22204e0ec5ed84a',
+        id: '223d25c297317463755043f276178aef',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
-        data: encoder.encode(backdrop)
+        data: encoder.encode(genTip(backdrop))
     }];
 };
 
