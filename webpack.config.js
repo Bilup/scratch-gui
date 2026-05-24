6b9924de81335ac01cac37ea288e1e75ed1e5786
RyaninCn11 <148440803+RyaninChina11@users.noreply.github.com>
2026-05-24 17:01:33 +0800

    fix todo

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ const base = {
         host: '0.0.0.0',
         disableHostCheck: true,
         compress: true,
-        port: process.env.PORT || 9178,
+        port: process.env.PORT || 8061,
         // allows ROUTING_STYLE=wildcard to work properly
         historyApiFallback: {
             rewrites: [
