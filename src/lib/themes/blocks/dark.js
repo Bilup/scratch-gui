3ebc32a4206988e6d127b213c3fecb1e71aa8f6d
DLGrass <2649771611@qq.com>
2026-05-30 12:06:02 +0800

    update block-theme(dark)

--- src/lib/themes/blocks/dark.js
+++ src/lib/themes/blocks/dark.js
@@ -4,73 +4,73 @@ const blockColors = {
     motion: {
         primary: '#0F1E33',
         secondary: '#4C4C4C',
-        tertiary: '#4C97FF',
+        tertiary: '#4C4C4C',
         quaternary: '#4C97FF'
     },
     looks: {
         primary: '#1E1433',
         secondary: '#4C4C4C',
-        tertiary: '#9966FF',
+        tertiary: '#4C4C4C',
         quaternary: '#9966FF'
     },
     sounds: {
         primary: '#291329',
         secondary: '#4C4C4C',
-        tertiary: '#CF63CF',
+        tertiary: '#4C4C4C',
         quaternary: '#CF63CF'
     },
     control: {
         primary: '#332205',
         secondary: '#4C4C4C',
-        tertiary: '#FFAB19',
+        tertiary: '#4C4C4C',
         quaternary: '#FFAB19'
     },
     event: {
         primary: '#332600',
         secondary: '#4C4C4C',
-        tertiary: '#FFBF00',
+        tertiary: '#4C4C4C',
         quaternary: '#FFBF00'
     },
     sensing: {
         primary: '#12232A',
         secondary: '#4C4C4C',
-        tertiary: '#5CB1D6',
+        tertiary: '#4C4C4C',
         quaternary: '#5CB1D6'
     },
     pen: {
         primary: '#03251C',
         secondary: '#4C4C4C',
-        tertiary: '#0fBD8C',
+        tertiary: '#4C4C4C',
         quaternary: '#0fBD8C'
     },
     operators: {
         primary: '#112611',
         secondary: '#4C4C4C',
-        tertiary: '#59C059',
+        tertiary: '#4C4C4C',
         quaternary: '#59C059'
     },
     data: {
         primary: '#331C05',
         secondary: '#4C4C4C',
-        tertiary: '#FF8C1A',
+        tertiary: '#4C4C4C',
         quaternary: '#FF8C1A'
     },
     data_lists: {
         primary: '#331405',
         secondary: '#4C4C4C',
-        tertiary: '#FF661A',
+        tertiary: '#4C4C4C',
         quaternary: '#FF661A'
     },
     more: {
         primary: '#331419',
         secondary: '#4C4C4C',
-        tertiary: '#FF6680',
+        tertiary: '#4C4C4C',
         quaternary: '#FF6680'
     },
     addons: {
         primary: '#0b3331',
         secondary: '#4C4C4C',
-        tertiary: '#34e4d0',
+        tertiary: '#4C4C4C',
         quaternary: '#34e4d0'
     },
     text: 'rgba(255, 255, 255, .7)',
@@ -88,7 +88,7 @@ const customExtensionColors = {
         return hsv2hex(hsv);
     },
     secondary: () => '#4C4C4C',
-    tertiary: primary => primary,
+    tertiary: () => '#4C4C4C',
     quaternary: primary => primary,
     categoryIconBackground: primary => customExtensionColors.primary(primary),
     categoryIconBorder: primary => customExtensionColors.tertiary(primary)
