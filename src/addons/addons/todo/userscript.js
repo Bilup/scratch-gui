a5af5cbb522101d475664186d399de15d297a888
DLGrass <2649771611@qq.com>
2026-06-11 12:36:40 +0800

    fix: update markdown & update translations

--- src/addons/addons/todo/userscript.js
+++ src/addons/addons/todo/userscript.js
@@ -128,7 +128,7 @@ export default async function ({ addon, msg }) {
     }
     const alpha = 'a0';
 
-    // 这个 ReduxStore 到底是哪里来的？？？
+    // 这个 ReduxStore 到底是哪里来的???
     await ReduxStore.subscribe(() => {
         PROJECT_NAME = ReduxStore.getState().scratchGui.projectTitle;
     })
