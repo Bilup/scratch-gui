6d2ee213a04578a84e0441557aef2201838b1c87
RyaninCn11 <148440803+RyaninChina11@users.noreply.github.com>
2026-07-06 14:35:41 +0800

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -105,8 +105,8 @@ const base = {
                 /node_modules[\\/]pify/,
                 /node_modules[\\/]@vernier[\\/]godirect/,
                 /node_modules[\\/]@chenglou[\\/]pretext/,
-                /node_modules[\\/]isomorphic-git/,
-                /node_modules[\\/]fractch[\\/]src/
+                /node_modules[\\/]fractch[\\/]src/,
+                /node_modules[\\/]isomorphic-git/
             ],
             options: {
                 // Explicitly disable babelrc so we don't catch various config
