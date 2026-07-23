ee60f86deec8909087cce3a723529961f660dc6e
RyaninCn11 <148440803+RyaninChina11@users.noreply.github.com>
2026-07-23 22:13:14 +0800

    fix: add trusted ext url

--- src/lib/trusted-extension.js
+++ src/lib/trusted-extension.js
@@ -1,5 +1,9 @@
 const isTrustedExtensionUrl = url => (
     url.startsWith('https://extensions.turbowarp.org/') ||
+    url.startsWith('https://extensions.bilup.org/') ||
+    url.startsWith('https://extensions.mistium.com/') ||
+    url.startsWith('https://sharkpools-extensions.vercel.app/') ||
+    url.startsWith('https://editors.astras.top/extensions/') ||
     url.startsWith('https://extensions.mistium.com/') ||
     url.startsWith('http://localhost:8000/')
 );
