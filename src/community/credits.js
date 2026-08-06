b014f521441d96935b47a5ef7045d54f768ef5cc
DLGrass <2649771611@qq.com>
2026-08-06 16:09:54 +0800

    feat: Ifdian

--- src/community/credits.js
+++ src/community/credits.js
@@ -1,12 +1,10 @@
-// Ko-fi credit top-up tiers (shared with the Bilup Accounts wallet). Buying sends the
-// credits to the account instantly once the Ko-fi order is processed.
 const PURCHASE_TIERS = [
-    {credits: 50, price: 1.99, link: 'https://ko-fi.com/s/eebeb7269f'},
-    {credits: 250, price: 8.99, link: 'https://ko-fi.com/s/0638cbfd65'},
-    {credits: 500, price: 15.99, link: 'https://ko-fi.com/s/e0ce188c8f'}
+    {credits: 50, price: 6.99, link: 'https://ifdian.net/order/create?product_type=1&plan_id=9336922490c911f1b6855254001e7c00&sku=%5B%7B%22sku_id%22%3A%22934012c290c911f1ac695254001e7c00%22,%22count%22%3A1%7D%5D'},
+    {credits: 200, price: 19.99, link: 'https://ifdian.net/order/create?product_type=1&plan_id=9336922490c911f1b6855254001e7c00&sku=%5B%7B%22sku_id%22%3A%229348610290c911f183e45254001e7c00%22,%22count%22%3A1%7D%5D'},
+    {credits: 500, price: 39.99, link: 'https://ifdian.net/order/create?product_type=1&plan_id=9336922490c911f1b6855254001e7c00&sku=%5B%7B%22sku_id%22%3A%22934f8c5c90c911f1baee5254001e7c00%22,%22count%22%3A1%7D%5D'}
 ];
 
-const KO_FI_SHOP_URL = 'https://ko-fi.com/mistium/shop';
+const KO_FI_SHOP_URL = 'https://ifdian.net/a/RyaninCn11';
 
 // Detect an "insufficient funds" failure from a Bilup Accounts transfer error.
 const isInsufficientFunds = error => {
